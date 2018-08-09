c35c4d8306646f4b8b4f592ee3bd33daa6d55ae0
wangxing <18912540921@163.com>
2018-08-09 14:26:41 +0800

    vich uploader bundle

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -22,6 +22,7 @@ class AppKernel extends Kernel
             new Knp\Bundle\PaginatorBundle\KnpPaginatorBundle(),
             new Knp\Bundle\MenuBundle\KnpMenuBundle(),
             new EasyCorp\Bundle\EasyAdminBundle\EasyAdminBundle(),
+            new Vich\UploaderBundle\VichUploaderBundle(),
 
         ];
 
